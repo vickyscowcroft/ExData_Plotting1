1393bcbdcaf799b3b6cdcc170cfe646ae5cbdcc3
Vicky Scowcroft <vickyscowcroft@gmail.com>
2015-05-09 17:55:50 -0700

    Adding plot2

---
+++ plot2.R
@@ -0,0 +1,26 @@
+## Plot 2 --Time vs Global active power
+## Construct the plot and save it to a PNG file with a width of 480 pixels and a height of 480 pixels
+## Output to plot2.png
+library(dplyr)
+library(lubridate)
+
+setwd("ExData_Plotting1")
+
+## Note that in this dataset missing values are coded as ?.
+# Read in the data, using the correct separator for the file and telling read.table about the file header
+data <- read.table("household_power_consumption.txt", sep=';', header=TRUE, na.strings='?')
+
+## We will only be using data from the dates 2007-02-01 and 2007-02-02.
+dataSubest <- subset(data, data$Date =='1/2/2007' | data$Date == '2/2/2007')
+
+## Conmbine the date and time columns to one dateTime variable
+dateTime = strptime(paste(dataSubset$Date, dataSubset$Time), format="%d/%m/%Y %H:%M:%S")
+
+## Plot dateTime vs. global active power
+plot(dateTime, dataSubset$Global_active_power, type='l', ylab="Global Active Power (kilowatts)", xlab="", cex.axis=0.8, cex.lab=0.8)
+
+## Save to plot2.png
+dev.copy(png, file = "plot2.png", width=480, height=480, units='px')
+
+## Close the output device
+dev.off()
